e3a93e723caa5f0c5f9d3fc409cc9ec7b4cac374
DackJempsey <jade8185@colorado.edu>
2018-03-22 16:02:28 -0600

    added files, updated notes

---
+++ SWEncode.c
@@ -0,0 +1,41 @@
+/*
+ * SWEncode.c
+ * 
+ * Copyright 2018 Jack Dempsey <jackdempsey@rgnt2-102-74-dhcp.int.colorado.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ * This is the encoding code/scheme used for this project. 
+ * This is to prepare the data so that the Rpi does not have to work as hard/
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	//first line of string should be ">sequence A - some descriptors then: /n the next line is the string"
+	
+	char Nucleotide = *argv;
+	
+	
+	return 0;
+}
+
