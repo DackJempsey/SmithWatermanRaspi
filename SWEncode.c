e76e01cc64885043806dffb1345effe562ade054
DackJempsey <jade8185@colorado.edu>
2018-04-02 11:44:25 -0600

    Changed output to a point to a file added while loop and switch statements Don’t forget to change name of file

--- SWEncode.c
+++ SWEncode.c
@@ -29,23 +29,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-FILE input(char *file)//takes in the file reads it, encodes it. outputs a file.
+FILE * input(char *file)//takes in the file reads it, encodes it. outputs a file.
 {
 	
-	FILE out; //output
-	out = fopen("FUck", "wb");
-	
+	FILE *out; //output
+	out = fopen("FUck.bin", "wb");
+	char outArr[10];//to use in the while loop.
 	//first line of string should be ">sequence A - some descriptors then: /n the next line is the string"
 	//create a file to put the encoding. make a new function
 	//char *file = argv[1]; //if you wanna take the file as an arguement
 	
 	
 	//put filename where test is, this is to directly load, then no need for argument.
+	FILE *fasta;
 	fasta  = fopen("test", "r");
 	
 	if(fasta == 0)
 	{
 		printf("Did not read");
+		exit(1);
+	}
+	else if (!out)
+	{
+		printf("Unable to open out file");
+		exit(1);
 	}
 	else
 	{	
@@ -53,11 +60,31 @@ FILE input(char *file)//takes in the file reads it, encodes it. outputs a file.
 		//first take the line ">stuf" and throw it away
 		
 		//char shit[100]; //this is the size file that will store some data, possibly do a mallo with stringlen 
-		while (fscanf (fasta, "%s", out) != EOF)
+		while (fscanf (fasta, "%s", outArr) != EOF)
 		{
 			// do stuff
 			//printf("%s",shit);
 			//ASII to binary file. then change to make it efficient.
+			char nuc;
+			fgets(nuc,1,fasta); //nucluetide from the line
+			switch(nuc) // convert nucluotide and store in out
+			{
+				case 'A':
+					//convert to 00
+					break;
+				case 'T':
+					//convert to 01
+					break;
+				case 'C':
+					//convert to 10
+					break;
+				case 'G':
+					//convert to 11
+					break;
+				default :
+					printf("%s", "Invalid character");
+				
+			}
 			
 		}
 	}
