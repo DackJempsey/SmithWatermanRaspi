f49e1f7ba315fabccdd5f939c499f7ad3882b349
DackJempsey <jade8185@colorado.edu>
2018-04-03 16:59:11 -0600

    Added encoding array and initialized it. made the switch cases nested. Added new function to prepare the file better for the encoding func. new func still needs a bunch of work.

--- SWEncode.c
+++ SWEncode.c
@@ -22,6 +22,7 @@
  * This is the encoding code/scheme used for this project. 
  * This is to prepare the data so that the Rpi does not have to work as hard/
  * 
+ * 
  */
 
 
@@ -29,22 +30,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-FILE * input(char *file)//takes in the file reads it, encodes it. outputs a file.
+FILE *prePrep(char *file)//this gets ride of the line deliniators and makes it all uppercase
+{	//first line of string should be ">sequence A - some descriptors then: /n the next line is the string"
+	
+
+	FILE *out; //output file
+	out = fopen("prePrep.bin" , "wb");
+	FILE *in;
+	in = fopen(file, "r");
+	
+	char Arr[100];
+	
+	 
+	getline(Arr, 1, in);
+	
+	
+	fclose(out);
+	fclose(in);
+	//free(); //frees buffer made by the getline.
+	
+	return out;
+}
+
+
+FILE * input(FILE *file)//takes in the file reads it, encodes it. outputs a file.
 {
 	
 	FILE *out; //output
-	out = fopen("FUck.bin", "wb");
-	char outArr[10];//to use in the while loop.
-	//first line of string should be ">sequence A - some descriptors then: /n the next line is the string"
+	out = fopen("Encoded.bin", "wb");
+	char outArr[12];//to use in the while loop.
+
 	//create a file to put the encoding. make a new function
+	
 	//char *file = argv[1]; //if you wanna take the file as an arguement
 	
+	char encode[16];
+	//initializing array for encoding
+	encode[0] = '0';
+	encode[1] = '1';
+	encode[2] = '2';
+	encode[3] = '3';	
+	
+	encode[4] = '4';
+	encode[5] = '5';
+	encode[6] = '6';
+	encode[7] = '7';
+	encode[8] = '8';
+	encode[9] = '9';
+		
+	encode[10] = 'A';
+	encode[11] = 'B';
+	encode[12] = 'C';
+	encode[13] = 'D';
+	encode[14] = 'E';
+	encode[15] = 'F';
+		
 	
 	//put filename where test is, this is to directly load, then no need for argument.
-	FILE *fasta;
-	fasta  = fopen("test", "r");
+	//FILE *fasta;
+	//fasta  = fopen( file, "r");
 	
-	if(fasta == 0)
+	if(file == 0)
 	{
 		printf("Did not read");
 		exit(1);
@@ -56,50 +102,119 @@ FILE * input(char *file)//takes in the file reads it, encodes it. outputs a file
 	}
 	else
 	{	
+		printf("Good Read file!\n");
 		//do parsing here
 		//first take the line ">stuff" and throw it away
 		
 		//char shit[100]; //this is the size file that will store some data, possibly do a mallo with stringlen 
 		int index =0;
-		while (fscanf (fasta, "%s", outArr) != EOF)
+		while (index<9)//fscanf (fasta, "%s", outArr) != EOF)
 		{
 			// do stuff
 			//printf("%s",shit);
 			//ASII to binary file. then change to make it efficient.
 			//char nuc;
-			fgets(outArr,1,fasta); //nucluetide from the line
+			fgets(&outArr[index],3,file); //nucluetide from the line
+
 			switch(outArr[index]) // convert nucluotide and store in out
 			{
-				//printf("%d", index); // print check
 				
 				case 'A':
-					//convert to 00
-					fwrite(00, 1/4, 1, out);
+					switch(outArr[index+1]){
+				
+						case 'A'://case aa
+							fwrite(&encode[0], 1, 1, out);
+							break;
+						case 'G'://case ag
+							fwrite(&encode[4], 1, 1, out);
+							break;
+						case 'C': //case ac
+							fwrite(&encode[8], 1, 1, out);
+							break;
+						case 'T': //case at
+							fwrite(&encode[12], 1, 1, out);
+							break;
+						default :
+							printf("%s", "Invalid character 1 ");
+					}
 					break;
-				case 'T':
-					//convert to 01
-					fwrite(01, 1/4, 1, out);
-					break;
-				case 'C':
-					//convert to 10
-					fwrite(10, 1/4, 1, out);
-					break;
+					
+					
 				case 'G':
-					//convert to 11
-					fwrite(11, 1/4, 1, out);
+
+					switch(outArr[index+1]){
+				
+						case 'A'://case ga
+							fwrite(&encode[1], 1, 1, out);
+							break;
+						case 'G'://case gg
+							fwrite(&encode[5], 1, 1, out);
+							break;
+						case 'C': //case gc
+							fwrite(&encode[9], 1, 1, out);
+							break;
+						case 'T': //case gt
+							fwrite(&encode[13], 1, 1, out);
+							break;
+						default :
+							printf("%s", "Invalid character 2 ");
+					}
 					break;
+		
+					
+				case 'C':
+					switch(outArr[index+1]){
+				
+						case 'A'://case ca
+							fwrite(&encode[2], 1, 1, out);
+							break;
+						case 'G'://case cg
+							fwrite(&encode[6], 1, 1, out);
+							break;
+						case 'C': //case cc
+							fwrite(&encode[10], 1, 1, out);
+							break;
+						case 'T': //case ct
+							fwrite(&encode[14], 1, 1, out);
+							break;
+						default :
+							printf("%s", "Invalid character 3 ");
+					}	
+					break;
+					
+				case 'T':
+					switch(outArr[index+1]){
+				
+						case 'A'://case ta
+							fwrite(&encode[3], 1, 1, out);
+							break;
+						case 'G'://case tg
+							fwrite(&encode[7], 1, 1, out);
+							break;
+						case 'C': //case tc
+							fwrite(&encode[11], 1, 1, out);
+							break;
+						case 'T': //case tt
+							fwrite(&encode[16], 1, 1, out);
+							break;
+						default :
+							printf("%s", "Invalid character 4 ");
+					}
+					break;				
+									
+															
 				default :
-					printf("%s", "Invalid character");
+					printf("%s", "Invalid character Final ");
 			
 			}
-			index+=1;
+			index+=2;
 			
 		}
 	}
 	
 
 	
-	fclose(fasta);
+	//fclose(fasta);
 	fclose(out);
 	
 	return out;
@@ -108,7 +223,7 @@ FILE * input(char *file)//takes in the file reads it, encodes it. outputs a file
 
 int main(int argc, char **argv)
 {	
-		input(*argv);
+		input(prePrep(*argv));
 
 	return 0;
 }
