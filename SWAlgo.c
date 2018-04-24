5f3969c5a28836e476c3e0d6821225ef84006ee3
DackJempsey <jade8185@colorado.edu>
2018-04-23 22:08:45 -0600

    Updated notes started to update SWAglo

--- SWAlgo.c
+++ SWAlgo.c
@@ -31,16 +31,64 @@
 
 struct stuc{
 
-		int smallArr[8][8];//this is the array that will be used for calculations.fixed
+		int smallArr[16][16];//this is the array that will be used for calculations.fixed
 		
 		int bigArrSize1;
 		int bigArrSize2;
 		
-		int **bigArr;		
-		//char bigArr[][];
+		int **bigArr;
 		
 		FILE *ret;
 };
+
+struct ret{
+	int score;
+	char from;//three options c - corner u-up l -left
+	
+};
+
+
+struct ret maxScore(int ij,int ji, int i1j1){
+	
+	struct ret out;
+
+	//i1j1 = i1j1;// + 3;
+	ij= ij-2;
+	ji=ji-2;
+	//or 0
+	
+	out.score = MAX(ji,MAX(ij,MAX(i1j1,0)));
+	
+
+	//could be the same score, but only from one place.
+	
+	if(out.score == i1j1){ out.from = 'c';}//always take corner case if possible.
+	else 
+	{	/*
+		if(out.score == ij && out.score == ji)//break tie later on
+		{
+			out.from = 'b';//both
+			printf("Both\n");
+		}
+		*/
+		if(out.score == ij)
+		{
+			out.from = 'u';//from up
+		}
+		else if(out.score == ji)
+		{
+			out.from = 'l';//from left
+		}
+		else
+		{
+			out.from = '0';//zero
+		}
+	}
+	
+	//printf("%d %d %d :: Score: %d From: %c  ",i1j1,ij,ji,out.score,out.from);
+	
+	return out;
+}
 
 void t(int i){
 	printf("test #%d \n",i);
@@ -52,47 +100,51 @@ FILE *alignment(FILE *nuc1, FILE *nuc2){//takes in files of encoded nucleotides 
 	
 	struct stuc align;
 	
-	align.bigArrSize1 = 100;
-	align.bigArrSize2 = 100;
+	nuc1=fopen("medTest1", "r");
+	nuc2=fopen("medTest2", "r");
+	
+	//getting size of files
+	fseek(nu1, 0L, SEEK_END);
+	align.bigArrSize2 = ftell(nuc1);
+	rewind(nuc1);
+	
+	fseek(nuc2,0L, SEEK_END);
+    align.bigArrSize2 = ftell(nuc2);
+    rewind(nuc2); 
 
-	align.bigArr = malloc((align.bigArrSize1)*sizeof(int*));
+
+	align.bigArr = calloc((align.bigArrSize1)*sizeof(int*));
 
 	for(int i=0;i<align.bigArrSize1-1;i++){
-		align.bigArr[i]= malloc((align.bigArrSize2+1)*sizeof(int));
-		printf("%d\n", *align.bigArr[i]);
+		align.bigArr[i]= calloc((align.bigArrSize2+1)*sizeof(int));
+		//printf("%d\n", *align.bigArr[i]);
 	}
 
 	align.ret = fopen("Score", "w");
-
-	//initialize smalArr
-	for(int j=0;j<8;j++){
-		align.smallArr[0][j] = 0;
-		}
-	for(int i=0;i<8;i++){
-		align.smallArr[i][0] = 0;
-	}
-
+	
 	//Rowmajor-> do rows first
 	for(int i=0;i<align.bigArrSize1;i++){
+		
 		for(int n=0;n<align.bigArrSize2;n++){
 
 			//takes a few at a time here:
 			//calculations here
-			for(int j=0;j<4;j++){
-				for(int k=0;k<4;k++){
+			
+			fgets(&smallArr,16,nuc1);//<-- need to change small arr and add more fgets.
+			
+			for(int j=0;j<16;j++){
+				for(int k=0;k<16;k++){
 					//look at the three surrounding matrix entries. pick lowest. or 0.
 					//do an OR to check if zero(meaning it has alignment). if it is not then pick algnemnt score 
 					
-					
 				}
 			}
-			printf("test\n");
 		//transfer to larger matrix here:
-		//this may be the bottleneck due to the transfer of data i.e. need to initialize smallArr for nex
+		//this may be the bottleneck due to the transfer of data i.e. need to initialize smallArr for next
 		//round so that it can use previous values.
 		
-			for(int j=0;j<4;j++){
-				for(int k=0;k<4;k++){
+			for(int j=0;j<16;j++){
+				for(int k=0;k<16;k++){
 					align.smallArr[k][j] = align.bigArr[align.bigArrSize2][align.bigArrSize1];
 				}
 			}
@@ -100,9 +152,11 @@ FILE *alignment(FILE *nuc1, FILE *nuc2){//takes in files of encoded nucleotides 
 		
 		}
 	}
-	printf("test2\n");
 	
+	fclose(nuc1);
+	fclose(nuc2);
 	fclose(align.ret);
+	
 	return align.ret;
 	
 }
